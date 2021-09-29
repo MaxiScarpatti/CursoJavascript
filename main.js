ae06f9b210c378575a1358ce311261d3f812151c
MaxiScarpatti <91625757+MaxiScarpatti@users.noreply.github.com>
2021-09-29 14:04:21 -0300

    Add files via upload
    Desafío 1 "Creación de algoritmo simple"

---
+++ main.js
@@ -0,0 +1,12 @@
+let nombre = prompt("Por favor ingrese su nombre");
+let apellido = prompt("Coloque su apellido");
+alert("Hola " + nombre + ' ' + apellido) 
+
+let edad = prompt("Ahora ingrese su edad");
+let nuestraedad = parseInt (edad)
+
+let numero = 20;
+
+let resultado = edad + numero;
+
+console.log(nuestraedad + 10);
