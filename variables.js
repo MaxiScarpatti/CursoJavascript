b5775a062619e2d47fc341e45c118f0292b0183f
Maximiliano Scarpatti <91625757+MaxiScarpatti@users.noreply.github.com>
2021-10-05 20:23:22 -0300

    Desafío 8 "Eventos"

--- variables.js
+++ variables.js
@@ -1,13 +1,66 @@
-const SelQuantity = 'Indicanos la cantidad que querés';
 
-let products = [
-    {id: 1, name: "Lemon Pie", price: 1250, stock: 20},
-    {id: 2, name: "Marquise", price: 1450, stock: 15},
-    {id: 3, name: "Pavlova", price: 1500, stock: 10},
-    {id: 4, name: "Selva Negra", price: 1600, stock: 10},
-    {id: 5, name: "Tiramisu", price: 1300, stock: 5},
-];
+class products {
+constructor (torta, cantidad, precio) {
+  this.torta = torta;
+  this.cantidad = cantidad;
+  this.precio = precio;
+  }
+}
+ let formulario = document.getElementById("formularioTorta");
+ formulario.addEventListener('submit', guardarTorta);
 
-let cart = 0;
+function cargarListado () {
+  let listadoTortas = JSON.parse(localStorage.getItem("listadoTortas"));
+  if (listadoTortas == null) {
+    return [];
+  }
+  return listadoTortas;
+}
 
-let resp = 'S';
+function armarTarjeta (elemento) {
+  const tarjeta = document.createElement("div");
+  tarjeta.classList.add("tarjeta");
+
+  const nombreTorta = document.createElement("h2");
+  nombreTorta.textContent = `${elemento.torta}`;
+  tarjeta.appendChild(nombreTorta);
+
+  const cantidad = document.createElement("div");
+  cantidad.textContent = `Cantidad: ${elemento.cantidad}`;
+  tarjeta.appendChild(cantidad);
+
+  const precio = document.createElement("div");
+  precio.textContent = `Precio: ${elemento.precio}`;
+  tarjeta.appendChild(precio);
+  return tarjeta;
+}
+
+function guardarListado (listadoTortas){
+  localStorage.setItem("listadoTortas", JSON.stringify(listadoTortas));
+  mostrarListado(listadoTortas);
+}
+
+function guardarTorta (elemento) {
+  e.preventDefault();
+  let nombre = document.querySelector("#torta").value;
+  let cantidad = document.querySelector("#cantidad").value;
+  let precio = document.querySelector("#precio").value;
+
+  let listadoTortas = cargarListado();
+
+  listadoTortas.push(new products (nombre, cantidad, precio));
+
+  guardarListado (listadoTortas);
+document.getElementById("formularioTorta").reset();
+}
+
+function mostrarListado (listadoTortas) {
+  let listado = document.getElementById("listado");
+  listado.textContent = "";
+  listadoTortas.map(elemento => {
+    listado.appendChild(armarTarjeta(elemento));
+  })
+
+}
+
+mostrarListado(cargarListado());
