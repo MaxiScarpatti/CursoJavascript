3a2005e0602780640c2f4e041cf1dcf4c8c40a62
Maximiliano Scarpatti <91625757+MaxiScarpatti@users.noreply.github.com>
2021-10-07 23:13:05 -0300

    Add files via upload
    2da entrega TF "ecommerce Pastelería"

--- main.js
+++ main.js
@@ -1,12 +1,43 @@
-let nombre = prompt("Por favor ingrese su nombre");
-let apellido = prompt("Coloque su apellido");
-alert("Hola " + nombre + ' ' + apellido) 
+class carrito {
+  comprarProducto(e){
+    e.preventDefault();
+    if(
+      e.target.classlist.contains(agregar-carrito)){
+        const producto = e.target.parentElement.parentElement;
+        this.leerDatosProducto(producto);
+      }
+  }
 
-let edad = prompt("Ahora ingrese su edad");
-let nuestraedad = parseInt (edad)
 
-let numero = 20;
+leerDatosProducto(producto){
+  const infoProducto = {
+    imagen : producto.querySelector('img').src,
+    titulo : producto.querySelector('h4').textContent,
+    precio : producto.querySelector('.precio span').textContent,
+       id : producto.querySelector('a').getAttribute('data-id'),
+       cantidad : 1
+  }
+  this.insertarCarrito(infoProducto);
+}
+insertarCarrito(producto){
+  const row = document.createElement('tr');
+  row.innerHTML = `
+<td>
+<img src= "${producto.imagen}" widht=100>
+</td>
+<td> ${producto.titulo}</td>
+<td> ${producto.precio}</td>
+<td> <a href ="#" class="borrar-producto" data-id="${producto.id}"</td>
+  `;
+listaProductos.appendChild(row);
+}
 
-let resultado = edad + numero;
-
-console.log(nuestraedad + 10);
+eliminarProducto(e){
+  e.preventDefault();
+  let producto, productoID;
+  if(e.target.classlist.contains('borrar-producto')){
+    e.target.parentElement.parentElement.remove();
+    producto = e.target.parentElement.parentElement;
+    productoID = producto.querySelector('a').getAttribute('data-id');
+  }
+}
